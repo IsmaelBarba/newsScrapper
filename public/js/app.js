1c0ea0a6722414f58d44e531dfc758c8c8aab5dd
Ismael Barba <ismaelbarba1@gmail.com>
2017-12-07 17:42:37 -0600

    commitment

---
+++ public/js/app.js
@@ -0,0 +1,61 @@
+
+$(document).on("click", ".scrapeNew", function (req, res) {
+    console.log("Clicked");
+    $.ajax({
+        method: "GET",
+        url: "/scrape"
+    })
+        .done(function (data) {
+            console.log(data);
+            location.reload();
+            alert("You have 30 New Articles")
+        })
+})
+
+$(document).on("click", ".saveBtn, .deleteBtn", function () {
+    console.log("Clicked");
+    var articleId = $(this).data("id");
+    console.log(articleId);
+    var bool = $(this).data("bool");
+    console.log(bool);
+    $.ajax({
+        method: "PUT",
+        url: "/articles/" + articleId + "/" + bool
+    })
+        .done(function (data) {
+            console.log(data);
+            location.reload();
+
+            if (data.saved) {
+                alert("You saved this article.");
+            } else {
+                alert("You deleted this article.");
+            }
+        })
+})
+
+
+//when the savenote button is clicked
+$(document).on("click", "#saveNote", function () {
+    //get id associated with article from submit button
+    var articleId = $(this).val();
+    console.log(articleId);
+
+    //run POST to change note with what's entered in the input
+    $.ajax({
+        method: "POST",
+        url: "/article/" + articleId,
+        data: {
+            //value from body input
+            body: $("#bodyinput").val()
+        }
+    })
+        .done(function (data) {
+            console.log(data);
+            //empty notes section
+
+            $("#notesModal").modal("hide");
+        });
+    //empty title and body for note
+    $("#bodyinput").val("");
+})
---
+++ server.js
@@ -0,0 +1,180 @@
+//dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+//scraping tools: works on client and server
+var request = require("request");
+var cheerio = require("cheerio");
+
+//require all models
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+//initialize express
+var app = express();
+
+//configure middleware
+
+// //use morgan logger for logging requests
+app.use(logger("dev"));
+
+//use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//sets up Handlebars app
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+//set mongoose to leverage built in JavaScript ES6 Promises
+//connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/scraper", {
+    useMongoClient: true
+});
+
+
+//Routes
+
+app.get("/scrape", function (req, res) {
+    request("https://news.ycombinator.com", function (error, response, html) {
+
+        if (!error && response.statusCode == 200) {
+            var $ = cheerio.load(html);
+            var result = [];
+            $('span.comhead').each(function (i, element) {
+                var a = $(this).prev();
+                //console.log(a.text());
+                var rank = a.parent().parent().text();
+                var title = a.text();
+                var url = a.attr('href');
+                var subtext = a.parent().parent().next().children('.subtext').children();
+                var points = $(subtext).eq(0).text();
+                var username = $(subtext).eq(1).text();
+                var comments = $(subtext).eq(2).text();
+                // Our parsed meta data object
+                var metadata = {
+                    rank: parseInt(rank),
+                    title: title,
+                    url: url,
+                    points: parseInt(points),
+                    username: username,
+                    comments: parseInt(comments)
+                }
+                result.push(metadata);
+                console.log(metadata);
+            })
+            //res.json(result);
+            //res.render("index", result);
+            db.Article
+                .create(result)
+                .then(function (dbArticle) {
+                    res.send(dbArticle);
+                    //res.json(result);
+                    //res.render("index", result);
+                })
+                .catch(function (err) {
+                    res.json(err);
+                })
+        }
+    });
+});
+
+app.get("/", function (req, res) {
+    db.Article
+        .find({})
+        .then(function (dbArticle) {
+            res.render("index", { article: dbArticle });
+        })
+        .catch(function (err) {
+            res.json(err);
+        })
+})
+
+
+
+app.get("/saved", function (req, res) {
+    db.Article
+        .find({ saved: true })
+        .then(function (dbArticle) {
+            res.render("saved", { article: dbArticle });
+        })
+        .catch(function (err) {
+            res.json(err);
+        })
+})
+
+app.get("/article/:id", function (req, res) {
+    db.Article
+        .findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        })
+})
+
+app.get("/articles", function (req, res) {
+    // db.Article
+    // 	.find({})
+    // 	.then(function(dbArticle) {
+    // 		res.json(dbArticle);
+    // 	})
+    // 	.catch(function(err) {
+    // 		res.json(err);
+    // 	})
+})
+
+app.put("/articles/:id/:bool", function (req, res) {
+    db.Article
+        .findOneAndUpdate({ _id: req.params.id }, { saved: req.params.bool }, { new: true })
+        .then(function (dbArticle) {
+            console.log("Saved Article");
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        })
+})
+
+app.post("/article/:id", function (req, res) {
+    db.Note
+        .create(req.body)
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true })
+        })
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        })
+})
+
+app.delete("/note/:nid/:aid", function (req, res) {
+    db.Article
+        .findOneAndRemove({ _id: req.params.id }, { save: req.params.bool }, { new: false })
+        .than(function (dbArticle) {
+            console.log("Deleted Article");
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        })
+})
+
+//start the server
+app.listen(PORT, function () {
+    console.log("App running on port" + PORT + "!");
+})
