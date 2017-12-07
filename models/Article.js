89b046c866b3410c23eae096b38c9b9a8e8a9a05
Ismael Barba <ismaelbarba1@gmail.com>
2017-12-07 17:51:03 -0600

    scrape away

---
+++ models/Article.js
@@ -0,0 +1,42 @@
+var mongoose = require("mongoose");
+
+//save reference to schema constructor
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    rank: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    points: {
+        type: Number,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    saved: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    note: [{
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }]
+});
+
+//creates model from above schema, using mongoose's model method
+var Article = mongoose.model("Article", ArticleSchema);
+
+//export Article model
+module.exports = Article;
---
+++ models/Note.js
@@ -0,0 +1,14 @@
+var mongoose = require("mongoose");
+
+//save reference to schema constructor
+var Schema = mongoose.Schema;
+
+var NoteSchema = new Schema({
+    body: {
+        type: String
+    }
+});
+
+var Note = mongoose.model("Note", NoteSchema);
+
+module.exports = Note;
